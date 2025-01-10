2f6f25cdaabd794175723a08fb71fa2547b47e5c
AgentOxygen <cameron.cummins@utexas.edu>
2025-01-10 12:01:52 -0600

    Updated to reflect import hierarchy
    Keeping old imports for testing, will remove later

--- cvdp/__init__.py
+++ cvdp/__init__.py
@@ -1,5 +1,10 @@
+from cvdp.cvdp import *
+import cvdp.diag as diag
+import cvdp.visualize as visualize
+import cvdp.io as io
+import cvdp.definitions as definitions
+
 from cvdp.lib import *
 from cvdp.plotting import *
 from cvdp.scripts import *
 from cvdp.utils import *
-import cvdp.definitions
