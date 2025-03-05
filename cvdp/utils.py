2ff25a0cf18493a17adcbb35353b507687985f67
AgentOxygen <cameron.cummins@utexas.edu>
2025-03-05 16:26:36 -0600

    Added various utility functions

--- cvdp/utils.py
+++ cvdp/utils.py
@@ -4,5 +4,18 @@ utils.py
 
 Utility functions used throughout the CVDP code base.
 """
+from time import time
+from importlib.metadata import version
+import datetime
+
+
 def log(msg: str):
     print(msg)
+
+
+def get_time_stamp():
+    return datetime.datetime.fromtimestamp(time()).strftime('%Y-%m-%d %H:%M')
+
+
+def get_version():
+    return version('cvdp')
